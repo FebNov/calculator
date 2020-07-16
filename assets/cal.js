0a02c16034c62759b4d08ed46096472f6eac4842
Hunterprey <595129026@qq.com>
2020-07-16 14:15:30 +1000

    Html & Css for calculator

---
+++ assets/cal.js
@@ -0,0 +1,18 @@
+"use strict";
+
+window.onload = function () {};
+var calculator = new Calculator();
+var display = document.querySelector(".calculator__display");
+
+display.innerHTML = 0;
+// class Calculator {
+//   constructor() {}
+//   listenEvent() {
+//     var numberListenser = document.querySelectorAll(".calculator__input");
+//     numberListenser.forEach(
+//         (number)=>{
+//             number.addEventListener('click' calculator.)
+//         }
+//     )
+//   }
+// }
