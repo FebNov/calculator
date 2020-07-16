e21d1641a248afefdd69c061df4900f77f5df539
Hunterprey <595129026@qq.com>
2020-07-16 16:49:04 +1000

    only display 0

--- assets/cal.js
+++ assets/cal.js
@@ -1,18 +1,46 @@
 "use strict";
+class Calculator {
+  // init the calculator
+  constructor() {
+    const display = document.querySelector(".calculator__display");
+    const operator = document.querySelectorAll(".operator");
+    const zeroStart = document.querySelector(".bigzero");
+    const clear = document.querySelector(".clear");
+    const numberListenser = document.querySelectorAll(".calculator__input");
+    let displayVal = 0;
+    display.innerHTML = displayVal;
+  }
 
-window.onload = function () {};
-var calculator = new Calculator();
-var display = document.querySelector(".calculator__display");
+  //add function for lisenEvent "click"
+  listenEvent() {
+    numberListenser;
+    return;
+  }
+  display() {
+    return;
+  }
 
-display.innerHTML = 0;
-// class Calculator {
-//   constructor() {}
-//   listenEvent() {
-//     var numberListenser = document.querySelectorAll(".calculator__input");
-//     numberListenser.forEach(
-//         (number)=>{
-//             number.addEventListener('click' calculator.)
-//         }
-//     )
-//   }
-// }
+  //each time press the button
+  updateScreen(num) {
+    return;
+  }
+  //run opeator
+  method() {
+    return;
+  }
+
+  //like "dot", only display once, or "operator" only work at last press
+  uniOperator() {
+    let checkUni = 0;
+    while (this.displayVal) return;
+  }
+  //reset to 0
+  clear() {
+    while (displayVal[displayVal.length - 1] === this.clear.innerHTML) {
+      this.displayVal = 0;
+    }
+  }
+}
+window.onload = function () {
+  const calculator = new Calculator();
+};
