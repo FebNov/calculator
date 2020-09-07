df97718fff5a0cf89300de9c2d03826256959b50
Hunterprey <595129026@qq.com>
2020-09-07 16:02:43 +1000

    complet dot method

--- assets/cal.js
+++ assets/cal.js
@@ -1,13 +1,14 @@
 "use strict";
 
-let checkUni = 0;
+let checkUniOperator = 0;
 let firstNumber = 0;
+let secondNumber = 0;
+let checkDot = 0;
 const display = document.querySelector(".screen");
 const operator = document.querySelectorAll(".operator");
-const zeroStart = document.querySelector(".zero");
-const clearC = document.querySelector(".clear");
+const stnad = document.querySelectorAll(".stand");
 const equal = document.querySelector(".equal");
-const dot = document.querySelectorAll(".dot");
+const dot = document.querySelector(".dot");
 const number = document.querySelectorAll(".number");
 
 var operatorLast = [];
@@ -19,30 +20,101 @@ number.forEach((number) => {
     setTimeout(() => {
       number.classList.remove("number--clicked");
     }, 100);
-
-    if (document.querySelector("operator--clicked")) {
-      firstNumber = display.value;
-      console.log(firstNumber);
-      display = 0;
-      document
-        .querySelector("operator--clicked")
-        .classList.remove("operator--clicked");
+    if (checkUniOperator != 0) {
+      display.value === "0"
+        ? (display.value = number.innerHTML)
+        : (display.value += number.innerHTML);
+    } else {
+      display.value === "0"
+        ? (display.value = number.innerHTML)
+        : (display.value += number.innerHTML);
     }
-    display.value += number.innerHTML;
   });
 });
+
+stnad.forEach((stand) => {
+  stand.addEventListener("click", () => {
+    switch (stand.innerHTML) {
+      case "C":
+        display.value = 0;
+        checkUniOperator = 0;
+        firstNumber = 0;
+        secondNumber = 0;
+        checkDot = 0;
+        break;
+      case "-/+":
+        display.value = display.value * -1;
+        break;
+      case "%":
+        display.value = display.value / 100;
+        break;
+    }
+  });
+});
+dot.addEventListener("click", () => {
+  if (checkDot != 0) {
+    return;
+  } else {
+    display.value += dot.innerHTML;
+    checkDot = 1;
+  }
+});
+
+equal.forEach((equal) => {
+  equal.addEventListener("click", () => {
+    display.value = firstNumber + secondNumber;
+    firstNumber = display.value;
+    secondNumber = 0;
+    checkUniOperator = 0;
+    checkDot = 0;
+  });
+});
+
 operator.forEach((operator) => {
   operator.addEventListener("click", () => {
     //console.log(display.value);
+    if ((checkUniOperator = 0)) {
+      switch (operator.innerHTML) {
+        case "+":
+          firstNumber = display.value;
+          checkUniOperator += 1;
+          break;
+        case "-":
+          firstNumber = display.value;
+          checkUniOperator += 1;
+          break;
+        case "*":
+          firstNumber = display.value;
+          checkUniOperator += 1;
+          break;
+        case "/":
+          firstNumber = display.value;
+          checkUniOperator += 1;
+          break;
+      }
+    } else {
+      switch (operator.innerHTML) {
+        case "+":
+          firstNumber = display.value;
+          checkUniOperator += 1;
+          break;
+        case "-":
+          firstNumber = display.value;
+          checkUniOperator += 1;
+          break;
+        case "*":
+          firstNumber = display.value;
+          checkUniOperator += 1;
+          break;
+        case "/":
+          firstNumber = display.value;
+          checkUniOperator += 1;
+          break;
+      }
+    }
     operator.classList.add("operator--clicked");
     setTimeout(() => {
       operator.classList.remove("operator--clicked");
     }, 100);
   });
-});
-
-dot.addEventListener("click", () => {
-  if (display.value.indexOf(".") === -1) {
-    display.value += dot.innerText;
-  }
 });
